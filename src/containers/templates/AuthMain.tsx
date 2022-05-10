1358466e1e076e05f5b5d3a09a75561f7062e5c0
andideve <andideve0@gmail.com>
2022-05-10 07:41:43 +0700

    feat: auth page redesign

---
+++ src/containers/templates/AuthMain.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Box, BoxProps } from '../../components/atoms/box';
+import media from '../../utils/media';
+
+export default function AuthMain({ children, ...rest }: Omit<BoxProps, 'sx'>) {
+  return (
+    <Box
+      as="main"
+      sx={{
+        padding: '2rem 4vw',
+        [media('lg')]: { paddingRight: '2vw', paddingLeft: '2vw' },
+      }}
+      {...rest}
+    >
+      {children}
+    </Box>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
+
+import AuthHeader from '../containers/templates/AuthHeader';
+import AuthMain from '../containers/templates/AuthMain';
+
+import { Container } from '../components/atoms/container';
+import { H1 } from '../components/atoms/headings';
+import { Box } from '../components/atoms/box';
+import { Paragraph } from '../components/atoms/typography';
+import { ButtonLink } from '../components/atoms/button';
 
 import API from '../services/api';
 
@@ -27,9 +35,35 @@ const Home: NextPage = () => {
   }, [router.isReady]);
 
   return (
-    <main style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Link href={SITE_PATHS.LOGIN_SPOTIFY}>Login to Spotify</Link>
-    </main>
+    <>
+      <AuthHeader>
+        <Container>
+          <H1 size="h2" sx={{ textAlign: 'center' }}>
+            Spotify Profile
+          </H1>
+        </Container>
+      </AuthHeader>
+      <Box sx={({ theme }) => ({ borderTopWidth: 1, borderColor: theme?.colors.border.default })} />
+      <AuthMain>
+        <Container maxWidth="sm">
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              textAlign: 'center',
+            }}
+          >
+            <Paragraph sx={{ marginBottom: '.75rem', fontWeight: 500 }}>
+              To continue, log in to Spotify.
+            </Paragraph>
+            <ButtonLink variant="filled" size="lg" href={SITE_PATHS.LOGIN_SPOTIFY}>
+              Log In to Spotify
+            </ButtonLink>
+          </Box>
+        </Container>
+      </AuthMain>
+    </>
   );
 };
 
