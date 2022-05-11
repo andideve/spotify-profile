5322e9e42ff0d315224fde71f6cb09b8e072bee6
andideve <andideve0@gmail.com>
2022-05-11 12:02:20 +0700

    feat: add `playlist-read-private` scope to spotify web api

--- src/config/spotify.ts
+++ src/config/spotify.ts
@@ -8,12 +8,18 @@ export const CLIENT_SECRET = process.env.CLIENT_SECRET || '';
 export const REDIRECT_URI = process.env.REDIRECT_URI || '';
 
 // token version needs to be upgraded if scope has been changed
-export const TOKEN_VERSION = 1;
+export const TOKEN_VERSION = 2;
 
 const authParameter: UserAuthParameter = {
   response_type: 'code',
   client_id: CLIENT_ID,
-  scope: createScope(['user-read-email', 'user-read-private', 'user-follow-read', 'user-top-read']),
+  scope: createScope([
+    'user-read-email',
+    'user-read-private',
+    'user-follow-read',
+    'user-top-read',
+    'playlist-read-private',
+  ]),
   redirect_uri: REDIRECT_URI,
 };
 
