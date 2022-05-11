e63e28642af971f2a9242d2b4aaa9062403787f5
andideve <andideve0@gmail.com>
2022-05-11 12:00:47 +0700

    configure protected paths

--- src/config/globals.ts
+++ src/config/globals.ts
@@ -1,3 +1,7 @@
+const ARTIST_PATH = '/artist';
+const PLAYLIST_PATH = '/playlist';
+const ALBUM_PATH = '/album';
+
 export const SITE_PATHS = {
   LOGIN_DASHBOARD: '/',
   LOGIN_SPOTIFY: '/login',
@@ -5,12 +9,20 @@ export const SITE_PATHS = {
   USER_TOP_TRACKS: '/user/top/tracks',
   USER_FOLLOWING: '/user/following',
   USER_PLAYLISTS: '/user/playlists',
-  ARTIST: (id: string | number) => `/artist?id=${id}`,
-  PLAYLIST: (id: string | number) => `/playlist?id=${id}`,
-  ALBUM: (id: string | number) => `/album?id=${id}`,
+  ARTIST: (id: string | number) => `${ARTIST_PATH}?id=${id}`,
+  PLAYLIST: (id: string | number) => `${PLAYLIST_PATH}?id=${id}`,
+  ALBUM: (id: string | number) => `${ALBUM_PATH}?id=${id}`,
 };
 
-export const PROTECTED_PATHS = [SITE_PATHS.USER_DASHBOARD];
+export const PROTECTED_PATHS = [
+  SITE_PATHS.USER_DASHBOARD,
+  SITE_PATHS.USER_TOP_TRACKS,
+  SITE_PATHS.USER_FOLLOWING,
+  SITE_PATHS.USER_PLAYLISTS,
+  ARTIST_PATH,
+  PLAYLIST_PATH,
+  ALBUM_PATH,
+];
 
 export const NAVBAR_WIDTHS = 74;
 export const NAVBAR_LG_WIDTHS = 100;
